f3d90d56f257bd7d7066d2463c15f5980f92c331
Chris Bracken <chris@bracken.jp>
2014-03-26 11:04:21 -0700

    Streamline timeout code.

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -7,21 +7,12 @@ library coverage.src.util;
 import 'dart:async';
 
 /// Retries the specified function with the specified interval and returns
-/// the result on successful completion. Optionally times out after the
-/// specified timeout.
-Future retry(Future f(), Duration interval, {Duration timeout: null}) {
-  var watch = new Stopwatch()..start();
+/// the result on successful completion.
+Future retry(Future f(), Duration interval) {
   var completer = new Completer();
   doRetry() {
-    f().then((result) {
-      completer.complete(result);
-    }, onError: (e) {
-      if (timeout != null && watch.elapsed > timeout) {
-        completer.completeError('Timed out after $timeout');
-      } else {
-        new Timer(interval, doRetry);
-      }
-    });
+    f().then((result) => completer.complete(result),
+        onError: (e) => new Timer(interval, doRetry));
   }
   doRetry();
   return completer.future;

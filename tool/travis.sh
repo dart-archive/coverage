69cc14805bb8e3ffea312bfb755a0af1dea181a6
Kevin Moore <kevmoo@google.com>
2015-03-27 15:22:25 -0700

    Configure Travis/Coveralls code coverage for package:coverage – so meta!

--- tool/travis.sh
+++ tool/travis.sh
@@ -21,3 +21,13 @@ if [ "$TRAVIS_DART_VERSION" = "stable" ]; then
   echo "Running tests..."
   pub run test
 fi
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/collect_coverage_test.dart
+fi

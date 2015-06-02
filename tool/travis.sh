d727dd4eb30fcb6da83905353339739cb26d1678
Kevin Moore <kevmoo@google.com>
2015-06-01 17:25:58 -0700

    update travis script to use COVERALLS_TOKEN environment var

--- tool/travis.sh
+++ tool/travis.sh
@@ -23,7 +23,6 @@ if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
   echo "Running coverage..."
   pub global activate dart_coveralls
   pub global run dart_coveralls report \
-    --token $COVERALLS_TOKEN \
     --retry 2 \
     --exclude-test-files \
     --debug \

df56290825fce833ea7bf3f4a949ed2eda2241bb
Kevin Moore <kevmoo@google.com>
2015-06-04 14:01:50 -0700

    use and validate wait-on-pause in collect_coverage test

--- test/test_files/test_app_isolate.dart
+++ test/test_files/test_app_isolate.dart
@@ -2,12 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
 import 'dart:isolate';
 
 /// The number of covered lines is tested and expected to be 4.
 ///
 /// If you modify this method, you may have to update the tests!
 void isolateTask(List threeThings) {
+  sleep(const Duration(milliseconds: 500));
+
   SendPort port = threeThings.first;
 
   var sum = threeThings[1] + threeThings[2];

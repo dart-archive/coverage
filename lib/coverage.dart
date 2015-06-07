8cd5a4eec13e0f067be534e6740a372c0ac85c8c
Kevin Moore <kevmoo@google.com>
2015-06-07 14:55:23 +0200

    coverage.dart: sort exports

--- lib/coverage.dart
+++ lib/coverage.dart
@@ -1,6 +1,6 @@
 library coverage;
 
-export 'src/hitmap.dart';
-export 'src/formatter.dart';
-export 'src/resolver.dart';
 export 'src/collect.dart';
+export 'src/formatter.dart';
+export 'src/hitmap.dart';
+export 'src/resolver.dart';

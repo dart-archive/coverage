55680e82b0707e5ab03180627f4f7f1c72a7bde9
Chris Bracken <chris@bracken.jp>
2017-10-25 14:52:15 -0700

    Test coverage for synchronous function call (#198)
    Adds a test to verify expected coverage is generated for a synchronous
    function with two branches, only one of which is executed.

--- test/test_files/test_app_isolate.dart
+++ test/test_files/test_app_isolate.dart
@@ -5,11 +5,20 @@
 import 'dart:io';
 import 'dart:isolate';
 
+String fooSync(int x) {
+  if (x == 42) {
+    return '*' * x;
+  }
+  return new List.generate(x, (_) => 'xyzzy').join(' ');
+}
+
 /// The number of covered lines is tested and expected to be 4.
 ///
 /// If you modify this method, you may have to update the tests!
 void isolateTask(List<dynamic> threeThings) {
   sleep(const Duration(milliseconds: 500));
+
+  fooSync(42);
 
   SendPort port = threeThings.first;
   int sum = threeThings[1] + threeThings[2];

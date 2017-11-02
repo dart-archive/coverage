7fb93da9f0722cc03884612374abfc6c64ccfdf2
Chris Bracken <chris@bracken.jp>
2017-11-02 13:39:33 -0700

    Add resolver tests to tests_all.dart (#206)

--- test/test_all.dart
+++ test/test_all.dart
@@ -3,6 +3,7 @@ import 'package:test/test.dart';
 import 'collect_coverage_api_test.dart' as collect_coverage_api;
 import 'collect_coverage_test.dart' as collect_coverage;
 import 'lcov_test.dart' as lcov;
+import 'resolver_test.dart' as resolver;
 import 'run_and_collect_test.dart' as run_and_collect;
 import 'util_test.dart' as util;
 
@@ -10,6 +11,7 @@ void main() {
   group('collect_coverage_api', collect_coverage_api.main);
   group('collect_coverage', collect_coverage.main);
   group('lcov', lcov.main);
+  group('resolver', resolver.main);
   group('run_and_collect', run_and_collect.main);
   group('util', util.main);
 }

317fff7820a6130511f7b9bfd6d5f895e1b82187
Chris Bracken <chris@bracken.jp>
2019-05-14 12:38:30 -0700

    Migrate to Dart 2 typedefs and parameter defaults
    Migrates the syntax introduced in Dart 2.0.0 for typedefs and optional
    parameter default values. Adds lints to prevent regressions.

--- lib/src/run_and_collect.dart
+++ lib/src/run_and_collect.dart
@@ -11,7 +11,7 @@ import 'util.dart';
 
 Future<Map<String, dynamic>> runAndCollect(String scriptPath,
     {List<String> scriptArgs,
-    bool checked: false,
+    bool checked = false,
     String packageRoot,
     Duration timeout}) async {
   var dartArgs = [

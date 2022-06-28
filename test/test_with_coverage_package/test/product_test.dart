d9d555f032e9ac595040f064515de181bf3de059
Kevin Moore <kevmoo@google.com>
2022-06-28 13:27:25 -0700

    Support passing extra arguments to test via test_with_coverage
    Fixes https://github.com/dart-lang/coverage/issues/403
    
    Also cleaned up help text

--- test/test_with_coverage_package/test/product_test.dart
+++ test/test_with_coverage_package/test/product_test.dart
@@ -2,4 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-void main() {}
+import 'package:test/test.dart';
+
+// ignore: avoid_relative_lib_imports
+import '../lib/validate_lib.dart';
+
+void main() {
+  test('product', () {
+    expect(product([2, 3]), 6);
+  });
+}
